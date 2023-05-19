e15078d450a073cc2878b1da0e6919a70d7a3abf
Mia-Guoo <mguo28@my.centennialcollege.ca>
2023-05-18 20:14:42 -0400

    Add security configuation

--- EasyVotes/src/main/java/com/easyvotes/EasyVotesApplication.java
+++ EasyVotes/src/main/java/com/easyvotes/EasyVotesApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 
-
 @SpringBootApplication
 public class EasyVotesApplication implements CommandLineRunner {
 //	@Autowired
